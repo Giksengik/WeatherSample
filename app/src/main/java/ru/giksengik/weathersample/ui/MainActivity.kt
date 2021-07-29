f563a37219d99c81e50b4c092bd0d9190b33430e
Giksengik <giksengik@gmail.com>
2021-07-29 10:39:02 +0300

    create branch

--- app/src/main/java/ru/giksengik/weathersample/ui/MainActivity.kt
+++ app/src/main/java/ru/giksengik/weathersample/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.giksengik.weathersample
+package ru.giksengik.weathersample.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.giksengik.weathersample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
