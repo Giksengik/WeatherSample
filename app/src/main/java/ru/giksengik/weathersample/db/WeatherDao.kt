046754065762ea32ce16704c58b99c385e4d582e
Giksengik <giksengik@gmail.com>
2021-08-02 17:35:26 +0300

    Add commands for weather DAO

--- app/src/main/java/ru/giksengik/weathersample/db/WeatherDao.kt
+++ app/src/main/java/ru/giksengik/weathersample/db/WeatherDao.kt
@@ -1,8 +1,22 @@
 package ru.giksengik.weathersample.db
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import ru.giksengik.weathersample.models.WeatherData
+import rx.Observable
+
 
 @Dao
 interface WeatherDao {
 
+    @Query("SELECT * FROM weatherdata")
+    fun getAll() : Observable<List<WeatherData>>
+
+    @Insert
+    fun insert(weatherData: WeatherData)
+
+    @Delete
+    fun delete(weatherData: WeatherData)
 }
